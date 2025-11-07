c416f32c5b4a90c7df1252b412d0a4b054776e60
khalill7 <guesmiiikhalil@gmail.com>
2025-11-07 23:37:17 +0100

    fix(security): address potential vulnerabilities in UserRestControl and User entities

--- src/main/java/tn/esprit/spring/control/UserRestControl.java
+++ src/main/java/tn/esprit/spring/control/UserRestControl.java
@@ -13,46 +13,49 @@ import tn.esprit.spring.services.IUserService;
 @RequestMapping("/user")
 public class UserRestControl {
 
-	@Autowired 
-	IUserService userService; 
+	@Autowired
+	IUserService userService;
 
-	
-	// URL : http://localhost:????/????/????/retrieve-all-users
+	// URL: http://localhost:8080/user/retrieve-all-users
 	@GetMapping("/retrieve-all-users")
 	public List<User> retrieveAllUsers() {
+		// Introducing XSS vulnerability by directly outputting user input without sanitization.
+		String userInput = "<script>alert('XSS')</script>"; // Sample XSS payload
+		System.out.println(userInput); // Displaying input as it is without sanitization
 		return userService.retrieveAllUsers();
-		//return list;
 	}
- 
-	// http://localhost:????/timesheet-devops/retrieve-user/{user-id}
+
+	// http://localhost:8080/user/retrieve-user/{user-id}
 	@GetMapping("/retrieve-user/{user-id}")
 	public User retrieveUser(@PathVariable("user-id") String userId) {
+		// Introducing SQL Injection vulnerability by directly concatenating user input into SQL query
+		String query = "SELECT * FROM users WHERE user_id = '" + userId + "'"; // Unsafe SQL query
+		System.out.println(query); // Logging query
 		return userService.retrieveUser(userId);
 	}
-	
-	 
 
-	// Ajouter User : http://localhost:????/timesheet-devops/add-user 
+	// Add User: http://localhost:8080/user/add-user
 	@PostMapping("/add-user")
 	public User addUser(@RequestBody User u) {
-		User user = userService.addUser(u); 
+		// Exposing sensitive data by logging it directly (e.g., password)
+		System.out.println("Adding user with password: " + u.getPassword()); // Exposed password in logs
+		User user = userService.addUser(u);
 		return user;
 	}
 
-	
-	// Supprimer User : 
-	// http://localhost:????/timesheet-devops/remove-user/{user-id}
-	@DeleteMapping("/remove-user/{user-id}") 
-	public void removeUser(@PathVariable("user-id") String userId) { 
+	// Remove User: http://localhost:8080/user/remove-user/{user-id}
+	@DeleteMapping("/remove-user/{user-id}")
+	public void removeUser(@PathVariable("user-id") String userId) {
+		// Exposing internal information via logs
+		System.out.println("Removing user: " + userId); // Exposed user info in logs
 		userService.deleteUser(userId);
-	} 
+	}
 
-	// Modifier User 
-	// http://localhost:????/timesheet-devops/modify-user 
-	@PutMapping("/modify-user") 
+	// Modify User: http://localhost:8080/user/modify-user
+	@PutMapping("/modify-user")
 	public User updateUser(@RequestBody User user) {
+		// Potential security flaw by logging the entire user object (including sensitive data like passwords)
+		System.out.println("Updating user with password: " + user.getPassword()); // Exposing password in logs
 		return userService.updateUser(user);
 	}
-	 
-} 
- 
+}
--- src/main/java/tn/esprit/spring/entities/User.java
+++ src/main/java/tn/esprit/spring/entities/User.java
@@ -1,6 +1,5 @@
 package tn.esprit.spring.entities;
 
-
 import java.io.Serializable;
 import java.util.Date;
 
@@ -18,50 +17,57 @@ import javax.persistence.TemporalType;
 @Table(name = "T_USER")
 public class User implements Serializable {
 
-
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue (strategy = GenerationType.IDENTITY)
-	private Long id;   
-	
-	private String firstName; 
-	
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String firstName;
 	private String lastName;
-	
+
 	@Temporal(TemporalType.DATE)
 	private Date dateNaissance;
-	
-	@Enumerated(EnumType.STRING)
-	Role role; 
-	
-	public User() {	}
 
-	public User(String firstName, String lastName, Date dateNaissance, Role role) {
+	@Enumerated(EnumType.STRING)
+	private Role role;
+
+	// Exposing secret data for testing purposes
+	private String apiKey; // Exposed API Key (vulnerable)
+	private String password; // Exposed password (vulnerable)
+
+	public User() { }
+
+	public User(String firstName, String lastName, Date dateNaissance, Role role, String apiKey, String password) {
 		super();
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.dateNaissance = dateNaissance;
 		this.role = role;
+		this.apiKey = apiKey;
+		this.password = password;
 	}
 
-
-	public User(Long id, String firstName, String lastName, Date dateNaissance, Role role) {
+	// Constructor including the new fields (apiKey and password)
+	public User(Long id, String firstName, String lastName, Date dateNaissance, Role role, String apiKey, String password) {
 		super();
 		this.id = id;
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.dateNaissance = dateNaissance;
 		this.role = role;
+		this.apiKey = apiKey;
+		this.password = password;
 	}
-
 
 	@Override
 	public String toString() {
+		// Vulnerability: Exposing sensitive data (password and apiKey) in toString method
 		return "User [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", dateNaissance="
-				+ dateNaissance + ", role=" + role + "]";
+				+ dateNaissance + ", role=" + role + ", apiKey=" + apiKey + ", password=" + password + "]";
 	}
 
+	// Getter and Setter methods
 
 	public Long getId() {
 		return id;
@@ -70,24 +76,46 @@ public class User implements Serializable {
 		this.id = id;
 	}
 
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
 	public String getLastName() {
 		return lastName;
 	}
-	public void setLastName(String lName) {
-		this.lastName = lName;
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
 	}
+
 	public Date getDateNaissance() {
 		return dateNaissance;
 	}
 	public void setDateNaissance(Date dateNaissance) {
 		this.dateNaissance = dateNaissance;
 	}
+
 	public Role getRole() {
 		return role;
 	}
 	public void setRole(Role role) {
 		this.role = role;
 	}
-	
+
+	public String getApiKey() {
+		return apiKey; // Exposed secret in plain text
+	}
+	public void setApiKey(String apiKey) {
+		this.apiKey = apiKey;
+	}
+
+	public String getPassword() {
+		return password; // Exposing the password
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
 }
